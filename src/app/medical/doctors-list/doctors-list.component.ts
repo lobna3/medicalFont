9c2e06135947416c93dd25f5ca6e9e448d5c9d9d
lobna3 <yousfi55lobna@gmail.com>
2021-04-24 14:27:47 +0100

    mesage

--- src/app/medical/doctors-list/doctors-list.component.ts
+++ src/app/medical/doctors-list/doctors-list.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/apis/auth.service';
+import { User } from 'src/app/viewModels/user';
 
+import { OrganisationService } from 'src/app/apis/organisation.service';
+import { Specialite } from 'src/app/viewModels/specialite';
 @Component({
   selector: 'app-doctors-list',
   templateUrl: './doctors-list.component.html',
   styleUrls: ['./doctors-list.component.css']
 })
 export class DoctorsListComponent implements OnInit {
-
-  constructor() { }
+  user: User = new User();
+  doctors: User[]
+  specialite: Specialite[]
+  constructor(private toastr: ToastrService, private router: Router, private api: AuthService,private orgapi:OrganisationService) { }
 
   ngOnInit(): void {
+    this.orgapi.getAll().subscribe((result:Specialite[])=>{
+      this.specialite= result
+    })
+    this.allDocors();
   }
+  server="http://localhost/blog/storage/app/"
 
+  allDocors() {
+    this.api.getAll()
+      .subscribe((result: User[]) => {
+        this.doctors = result
+        console.log(result)
+      }, (error: any) => {
+        console.log(error)
+        this.toastr.error(error.error.msg);
+      })
+
+  }
 }
